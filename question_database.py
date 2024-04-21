f74293b0d1eb2e5af8da0d63bf8edd3048630797
Peter Zorve <98400766+peterzorve@users.noreply.github.com>
2024-04-21 16:17:11 +0300

    Add files via upload

---
+++ question_database.py
@@ -0,0 +1,43 @@
+all_qustions = [
+  {
+    "question": "What is the capital of Finland?", 
+    "A": "Helsinki",
+    "B": "Toronto",
+    "C": "Paris", 
+    "D": "London", 
+    "correct_option": "A"
+  },  
+  {
+    "question": "How many continents are in the world?", 
+    "A": "5",
+    "B": "6",
+    "C": "7", 
+    "D": "8", 
+    "correct_option": "C"
+  }, 
+  {
+    "question": "What is the capital of Nigeria?", 
+    "A": "Lagos",
+    "B": "Abuja",
+    "C": "Lome", 
+    "D": "Accra", 
+    "correct_option": "B"
+  }, 
+  {
+    "question": "Which of the following is not a web browser", 
+    "A": "Google Chrome",
+    "B": "Mozilla Firefox",
+    "C": "Microsoft Edge", 
+    "D": "Javascript", 
+    "correct_option": "B"
+  }, 
+  {
+    "question": "WHich of the following is a programming language", 
+    "A": "Python",
+    "B": "Data Science",
+    "C": "Artificial Intelligence", 
+    "D": "Chemistry", 
+    "correct_option": "A"
+  }, 
+
+]
